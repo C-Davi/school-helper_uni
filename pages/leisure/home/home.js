3367535a1055ac4a775d1128cd01f105b6238fee
cbw <791878868@qq.com>
2019-09-16 17:31:00 +0800

    切换选项缓存问题

--- pages/leisure/home/home.js
+++ pages/leisure/home/home.js
@@ -6,7 +6,9 @@ Page({
         isActive: 0,
         nav: [ "身心社团", "技能社团" ],
         news: "",
-        type:1
+        type:1,
+        md:"",
+        en:""
     },
     onLoad: function() {
       
@@ -23,20 +25,43 @@ Page({
     },
     getTeamsByType:function(){
       let teamType = this.data.type;
+      console.log(teamType);
       wx.showLoading({
         title: '加载中',
       })
-      team.getTeamsByType(teamType,(res)=>{
-        console.log(res)
-        if(res.status ==1){
-          let img_pre = res.data.img_pre;
-          let list = res.data.list;
+      if(this.data.news!=''){
+        if(teamType==1){
           this.setData({
-            img_pre: img_pre,
-            news:list
+            news:this.data.md
+          })
+        }else if(teamType==2){
+          this.setData({
+            news:this.data.en
           })
         }
-      })
+      }else{
+        team.getTeamsByType(teamType, (res) => {
+          console.log(res)
+          if (res.status == 1) {
+            let img_pre = res.data.img_pre;
+            let list = res.data.list;
+            this.setData({
+              img_pre: img_pre,
+              news: list
+            })
+            if (teamType == 1) {
+              this.setData({
+                md: list
+              })
+            } else if (teamType == 2) {
+              this.setData({
+                en: list
+              })
+            }
+          }
+        })
+      }
+      
       setTimeout(function () {
         wx.hideLoading()
       }, 1500);
